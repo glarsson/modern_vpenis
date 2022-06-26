c248dc032e8c56e9fd0e7d3498e1f3dc80c63fb8
Gerry Larsson <gerry.larsson@gmail.com>
2022-06-26 04:10:17 +0200

    Proper no internet testing implementation 10

--- modernvpenis.sh
+++ modernvpenis.sh
@@ -129,7 +129,7 @@ echo -e "\033[0m"
 
 # Internet speed details
 # check if defined
-if ENABLE_INTERNET_SPEED_TEST ; then
+if $ENABLE_INTERNET_SPEED_TEST ; then
 echo -e "${NC}INTERNET DETAILS (if INTERNET_TESTS specifically defined, speed needs separate package)"
 
 # run speedtest (external package) to gather internet speeds based on its defaults
